4f9dfdd0c1795e009d6ecd033fa20230d7cde5ef
wantstolearn <wantstolearn@users.noreply.github.com>
2014-11-01 15:09:05 -0400

    Update integer_input_check.rb

--- integer_input_check.rb
+++ integer_input_check.rb
@@ -2,10 +2,11 @@ input = nil # have to give it an initial  vlaue of until wont work on first atte
 
 attempts_left = 6
 	
-until input.is_a?(Integer) or attempts_left == 0  do
-   print "Please enter a number: "
-    attempts_left = attempts_left-1
-   puts "you have #{attempts_left} remaining to enter a number"
+until input.is_a?(Integer) or attempts_left == 1  do
+  
+   attempts_left = attempts_left-1
+   print "you have #{attempts_left} remaining to enter a number\n"
+   print "Please enter a number: \n"
    input = Integer(gets) rescue " - no wait, the computer tells me you never typed a number!" 
 end 
 	
