fc0f509c139907f477af04854cd843116b36c486
wantstolearn <wantstolearn@users.noreply.github.com>
2014-11-02 18:54:06 -0500

    Update integer_input_check.rb

--- integer_input_check.rb
+++ integer_input_check.rb
@@ -5,7 +5,7 @@ attempts_left = 6
 until input.is_a?(Integer) or attempts_left == 1  do
   
    attempts_left = attempts_left-1
-   print "you have #{attempts_left} remaining to enter a number\n"
+   print "you have #{attempts_left} remaining chances to enter a number\n"
    print "Please enter a number: \n"
    input = Integer(gets) rescue " - no wait, the computer tells me you never typed a number!" 
 end 
